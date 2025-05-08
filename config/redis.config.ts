efe736973fc628b5a131c6cbf6c50fd81dc704af
vinothkumarkuppusamy <16bc23@gmail.com>
2025-05-08 12:12:15 +0530

    redis ready to live

--- config/redis.config.ts
+++ config/redis.config.ts
@@ -9,10 +9,12 @@ const redisClient = createClient({
 redisClient.on("error", (err) => console.error("Redis Client Error", err));
 
 (async () => {
+  try {
     await redisClient.connect();
-    console.log(">>>>>>", process.env.REDIS_URL)
     console.log("Connected to Redis");
-  
+  } catch (err) {
+    console.error("Redis connection failed:", err);
+  }
 })();
 
 export default redisClient;
