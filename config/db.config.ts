038a73ea7ff9fe6d2fbbb48030a8e41d6cee9206
vinothkumarkuppusamy <125344744+vinothkumarkuppusamy@users.noreply.github.com>
2025-11-06 16:31:59 +0530

    Update db.config.ts

--- config/db.config.ts
+++ config/db.config.ts
@@ -4,7 +4,7 @@ const connectDB = async (): Promise<void> => {
   try {
     const mongoURI =
       process.env.MONGO_URI ??
-      "mongodb+srv://nodeTask:Admin%40123@cluster0.sschk9z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+      "";
     await mongoose.connect(mongoURI);
     console.log("MongoDB connected");
   } catch (error) {
